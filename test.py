431283afaa77fcb5d61ea147a5f8091a1b302848
dcazabat <dacazabat@gmail.com>
2022-05-24 16:36:24 -0300

    Add, Update; Delete & Select

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+# Ejemplo SQLAlchemy adapatado de la documentación del mismo+
+# No posee validaciones si el dato a modificar o borrar no existe 
+# o bien si al agregar ya existe el dato, lo repite, en las cosnultas
+# con "one()" si hay mas de un dato da error.
+
+from ast import stmt
 from models import User, Address
 from db import engine, Base
 
@@ -25,7 +31,7 @@ def insert_data(engine):
         session.add_all([spongebob, sandy, patrick])
         session.commit()
 
-def select_data(engine):
+def select_all_data(engine):
 
     with Session(engine) as session:
 
@@ -34,12 +40,64 @@ def select_data(engine):
         for user in session.scalars(stmt):
             print(user)
 
+def select_filter_data(engine, name='', address=''):
+
+    with Session(engine) as session:
+        stmt = (
+            select(Address)
+                .join(Address.user)
+                .where(User.name == name)
+                .where(Address.email_address == address)
+        )
+        return session.scalars(stmt).one()
+
+def modify_data(engine):
+
+    with Session(engine) as session:
+
+        # Actualizar un dato de la tabla User, de forma directa, agregando a la tabla Address un mail
+        stmt = select(User).where(User.name == "patrick")
+        patrick = session.scalars(stmt).one()
+        patrick.addresses.append(
+            Address(email_address="patrickstar@sqlalchemy.org")
+        )
+
+        session.commit()
+
+
+def delete_data_other_table(engine):
+
+    with Session(engine) as session:
+
+        stmt = (
+            select(Address)
+                .join(Address.user)
+                .where(User.name == "sandy")
+                .where(Address.email_address == "sandy@sqlalchemy.org")
+        )
+        sandy_address = session.scalars(stmt).one()
+        sandy = session.get(User, sandy_address.user_id)
+        sandy.addresses.remove(sandy_address)
+        session.commit()
+
+def delete_user(engine):
+    with Session(engine) as session:
+        stmt = select(User).where(User.name == "patrick")
+        patrick = session.scalars(stmt).one()   
+        print(f'Delete {patrick}')
+        session.delete(patrick)
+        session.commit()
 
 def run():
     # insert_data(engine=engine)
-    select_data(engine=engine)
+    select_all_data(engine=engine)
+    # print(select_filter_data(engine=engine, name='sandy', address='sandy@sqlalchemy.org'))
+    # modify_data(engine=engine)
+    delete_user(engine=engine)
+    # delete_data_other_table(engine=engine)
     pass
 
 if __name__ == '__main__':
+    # Crea las Tablas, pero si existe no las borra o sobreescribe
     Base.metadata.create_all(engine)
     run()
